734dfb386bed4606cbdc08ed0e1a7f9f2d88fee4
Andrey Oliveira <andreyq.oliveira@gmail.com>
2023-02-22 19:39:25 -0300

    adds /videos routing

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const connectDB = require("./db");
 const usersRoutes = require("./routes/users");
+const videosRoutes = require("./routes/videos");
 
 // Start the database connection
 connectDB();
 
 // Routes
 usersRoutes(app);
+app.use("/videos", videosRoutes);
 
 // Start the server
 port = process.env.PORT || 5000;
