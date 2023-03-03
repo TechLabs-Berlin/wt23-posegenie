30001bbcab015650a7e226a60844867f320af89c
Andrey Oliveira <andreyq.oliveira@gmail.com>
2023-03-02 21:36:34 -0300

    adds video sending to /process_video

--- backend/routes/videos.js
+++ backend/routes/videos.js
@@ -3,16 +3,25 @@ const router = express.Router();
 
 const multer = require("multer");
 const upload = multer({ dest: "./uploads" });
+const axios = require("axios");
 
-router.post("/upload", upload.single("video"), (req, res) => {
+router.post("/upload", upload.single("video"), async (req, res) => {
     console.log("/videos/upload POST request");
+    url = "http://localhost:6000/process_video";
     console.log(req.file);
-    res.send(req.body);
+    axios
+        .post(url, req.file)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+    res.send(req.file);
 });
 
 router.post("/upload-feedback", upload.single("video"), (req, res) => {
     console.log("/videos/upload-feedback POST request");
-    console.log(req.file);
     res.send(req.body);
 });
 
