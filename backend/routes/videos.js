3f5fc07a314918a11bd22c32ca709858cacbbadd
Andrey Oliveira <andreyq.oliveira@gmail.com>
2023-03-04 10:20:39 -0300

    redirects video from frontend to flask server

--- backend/routes/videos.js
+++ backend/routes/videos.js
@@ -2,22 +2,34 @@ const express = require("express");
 const router = express.Router();
 
 const multer = require("multer");
-const upload = multer({ dest: "./uploads" });
+const upload = multer();
 const axios = require("axios");
+const FormData = require("form-data");
 
-router.post("/upload", upload.single("video"), async (req, res) => {
+router.post("/upload", upload.single("video"), (req, res) => {
     console.log("/videos/upload POST request");
+    video = req.file;
+    console.log(video);
+
+    // create FormData object and append the file data to it
+    const formData = new FormData();
+    formData.append("file", video.buffer, {
+        filename: video.originalname,
+        contentType: video.mimetype,
+    });
+
+    // Send the video to flask server
     url = "http://localhost:6000/process_video";
-    console.log(req.file);
     axios
-        .post(url, req.file)
-        .then((res) => {
-            console.log(res);
+        .post(url, formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
         })
-        .catch((err) => {
-            console.log(err.message);
-        });
-    res.send(req.file);
+        .then((res) => console.log(res.data));
+
+    // Send the response to frontend
+    res.send(video);
 });
 
 router.post("/upload-feedback", upload.single("video"), (req, res) => {
