d35e6931c770a6150163ba86c0fd56db278b2837
Andrey Oliveira <58258823+Andreyqo@users.noreply.github.com>
2023-03-16 14:34:04 -0300

    changes port to 5000

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ usersRoutes(app);
 app.use("/videos", videosRoutes);
 
 // Start the server
-port = process.env.PORT || 4000;
+port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Backend server running on port ${port}`);
 });
