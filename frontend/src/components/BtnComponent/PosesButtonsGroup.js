a6fa931d8413f99e6ac9525300fa89a82fd7e466
Iryna Zhebryk <irinazhebrik@gmail.com>
2023-03-23 13:22:57 +0100

    feat: poses description

--- frontend/src/components/BtnComponent/PosesButtonsGroup.js
+++ frontend/src/components/BtnComponent/PosesButtonsGroup.js
@@ -1,37 +1,51 @@
 import React, { useState } from "react";
 import "./ButtonComponent.css";
+import WarriorComponent from "./WarriorComponent";
+import LungesComponent from "./LungesComponent";
 
 function PosesButtonsGroup({ onButtonClicked }) {
-    const [activeButton, setActiveButton] = useState(null);
-    // TODO: get poses from the backend
-    const buttons = [
-        { id: "lunges", label: "Lunges" },
-        { id: "warrior", label: "Warrior" },
-    ];
+  const [activeButton, setActiveButton] = useState(null);
+  const [selectedPose, setSelectedPose] = useState(null);
+  // TODO: get poses from the backend
+  const buttons = [
+    { id: "lunges", label: "Lunges" },
+    { id: "warrior", label: "Warrior" },
+  ];
 
-    const handleButtonClick = (value) => {
-        setActiveButton(value);
-        onButtonClicked(value);
-    };
+  const handleButtonClick = (value) => {
+    setActiveButton(value);
+    onButtonClicked(value);
+    setSelectedPose(value);
+  };
 
-    return (
-        <div className="container">
-            <div>
-                <h2>Choose an exercise</h2>
-            </div>
-            <div className="btn_container">
-                {buttons.map((button) => (
-                    <button
-                        key={button.id}
-                        onClick={() => handleButtonClick(button.id)}
-                        className={activeButton === button.id ? "active" : ""}
-                    >
-                        {button.label}
-                    </button>
-                ))}
-            </div>
-        </div>
-    );
+  const renderPoseComponent = () => {
+    if (selectedPose === "lunges") {
+      return <LungesComponent />;
+    } else if (selectedPose === "warrior") {
+      return <WarriorComponent />;
+    }
+    return null;
+  };
+
+  return (
+    <div className="container">
+      <div>
+        <h2>Choose an exercise</h2>
+      </div>
+      <div className="btn_container">
+        {buttons.map((button) => (
+          <button
+            key={button.id}
+            onClick={() => handleButtonClick(button.id)}
+            className={activeButton === button.id ? "active" : ""}
+          >
+            {button.label}
+          </button>
+        ))}
+      </div>
+      {renderPoseComponent()}
+    </div>
+  );
 }
 
 export default PosesButtonsGroup;
---
+++ frontend/src/components/BtnComponent/WarriorComponent.js
@@ -0,0 +1,20 @@
+function WarriorComponent() {
+  return (
+    <div className="recomdetion-container">
+      <h3 className="recomendation-title">Warrior pose</h3>
+      <p className="recomendation-p">
+        Begin standing, then step your right foot forward about four feet. With
+        your foot parallel and toes pointing to the top of the mat, bend your
+        knee into a lunge. Keep your left leg straight behind you and turn your
+        left heel in at approximately 45 degrees. Raise your arms straight above
+        your head, keeping your shoulders pressed down.
+      </p>
+      <p className="recomendation-p">
+        Squeeze your shoulder blades together and downward, and lift your chin
+        to gaze at your hands overhead. Hold your pose and then repeat on the
+        left side.
+      </p>
+    </div>
+  );
+}
+export default WarriorComponent;
